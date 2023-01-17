20eadfa70b6c3e256f2f9087316e9c0ecb60c417
fabiansf01 <110618996+fabiansf01@users.noreply.github.com>
2023-01-17 20:38:14 -0300

    img

---
+++ src/miciudad_android.php
@@ -0,0 +1,6 @@
+<?php
+echo  "https://play.google.com/store/apps/details?id=appinventor.ai_fabianperaltasfco.Miciudad" . "br";
+echo  "fabianperaltasfco@gmail.com.ar"  . "br";
+echo  "+54 9 3564 560635 "
+
+?>
